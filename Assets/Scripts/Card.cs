aa3f69f8ae2039dad8730cad25c53d41520e6293
Unknown <hdgbdn92@gmail.com>
2018-08-24 00:29:04 +0800

    暂时给脚本开了个头
    Add some properties to card item

---
+++ Assets/Scripts/Card.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Card : MonoBehaviour
+{
+    public int cardId;
+
+    public enum CardQuality
+    {
+        Gray,
+        White,
+        Green,
+        Purple,
+        Orange,
+    };
+
+    public uint cardCost;
+
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
